9757719db2aed7e334107a067dcf7ca7dff50378
piyopanman <g19911kh@gm.tsuda.ac.jp>
2024-02-25 14:36:25 +0900

    add meta object in switch-case-order.js

---
+++ switch-case-ascending.js
@@ -0,0 +1,16 @@
+module.exports = {
+  meta: {
+    type: "layout",
+    docs: {
+      description:
+        "Enforce that case labels in switch statements are in ascending alphabetical order.",
+      url: "",
+    },
+    schema: [],
+  },
+  create(context) {
+    return {
+      // TODO: add callback function(s)
+    };
+  },
+};
